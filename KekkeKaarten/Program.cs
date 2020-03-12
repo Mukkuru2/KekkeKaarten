3d2b9724dc193a5bc27f86c3e1a8bef236a5ac68
Stien_the_Troller <stijnkaay@gmail.com>
2020-03-12 17:06:53 +0100

    pls werk

--- KekkeKaarten/Program.cs
+++ KekkeKaarten/Program.cs
@@ -14,7 +14,7 @@ namespace KekkeKaarten
         [STAThread]
         static void Main()
         {
-            using (var game = new Game1())
+            using (var game = new KekkeKaarten())
                 game.Run();
         }
     }
