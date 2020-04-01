6596786c69c7bd66701d5f06f766a219174c9553
Stien_the_Troller <stijnkaay@gmail.com>
2020-04-01 12:22:14 +0200

    add kaart

---
+++ Centipede/GameObjects/Kaart.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.Xna.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Centipede.GameObjects
+{
+    class Kaart : SpriteGameObject
+    {
+        Kaart(): base("Kaart")
+        {
+            position.X = 500;
+            position.Y = 500;
+        
+        }
+
+        public override void Update(GameTime gameTime)
+        {
+            base.Update(gameTime);
+        }
+    }
+}
