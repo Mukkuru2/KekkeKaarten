21a1154541c8920a48381d14a73aaff62633ddcb
remcospil1 <remcospil1@gmail.com>
2020-04-01 12:39:38 +0200

    GameOverstate, PlayingState, StartState

---
+++ Centipede/GameState/GameOverState.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+
+namespace Centipede
+{
+    class GameOverState : GameObjectList
+    {
+        public GameOverState()
+        {
+            this.Add(new SpriteGameObject("YouDied"));
+        }
+
+        public override void HandleInput(InputHelper inputHelper)
+        {
+            base.HandleInput(inputHelper);
+            if (inputHelper.KeyPressed(Keys.Space))
+            {
+                GameEnvironment.GameStateManager.SwitchTo("StartState");
+            }
+        }
+    }
+}  
