6878ba15af4c39635cf8f02d1e18074674fdc7fb
Mukkuru2 <dorusvanweelden@gmail.com>
2020-04-01 13:29:35 +0200

    Removed Centipede Namespace

--- KekkeKaarten/GameObjects/Kaart.cs
+++ KekkeKaarten/GameObjects/Kaart.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Centipede.GameObjects
+namespace KekkeKaarten.GameObjects
 {
     class Kaart : SpriteGameObject
     {
--- KekkeKaarten/KekkeKaarten.cs
+++ KekkeKaarten/KekkeKaarten.cs
@@ -1,4 +1,4 @@
-﻿using Centipede;
+﻿using KekkeKaarten;
 using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
 using Microsoft.Xna.Framework.Input;
