770199b4ac184cc3c2bd4c392d3e44e1cefedf85
Mukkuru2 <dorusvanweelden@gmail.com>
2020-04-03 01:43:02 +0200

    CSV works YEET

---
+++ KekkeKaarten/CSVhandling/CSVimporter.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.VisualBasic.FileIO;
+
+
+namespace KekkeKaarten.CSVhandling
+{
+    public static class CSVimporter
+    {
+        static string path = @"CSVhandling/missions_rekenen_export.csv";
+        static TextFieldParser csvParser = new TextFieldParser(path);
+
+        public static List<List<string>> GetCSV()
+        {
+
+            csvParser.CommentTokens = new string[] { "#" };
+            csvParser.SetDelimiters(new string[] { ";" });
+            csvParser.HasFieldsEnclosedInQuotes = true;
+
+            List<List<string>> fullList = new List<List<string>>(); ;
+
+            //mission_id;id;Question type;Question;Did you know?;Difficulty level;Display type;Verification status;Comments;Answers
+            List<string> missionID = new List<string>();
+            List<string> id = new List<string>();
+            List<string> question = new List<string>();
+            List<string> DYK = new List<string>();
+            List<string> difficulty = new List<string>();
+            List<string> comments = new List<string>();
+            List<string> correctanswer = new List<string>();
+
+            csvParser.ReadLine();
+
+            while (!csvParser.EndOfData)
+            {
+                string[] values = csvParser.ReadFields();
+
+                missionID.Add(values[0]);
+                id.Add(values[1]);
+                question.Add(values[3]);
+                DYK.Add(values[4]);
+                difficulty.Add(values[5]);
+                comments.Add(values[8]);
+                correctanswer.Add(values[9]);
+            }
+            return fullList;
+        }
+    }
+}
--- KekkeKaarten/GameState/StartState.cs
+++ KekkeKaarten/GameState/StartState.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using KekkeKaarten.CSVhandling;
 using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
 using Microsoft.Xna.Framework.Input;
@@ -13,6 +14,7 @@ namespace KekkeKaarten
     {
         public StartState()
         {
+            CSVimporter.GetCSV();
             this.Add(new SpriteGameObject("IntroScreen"));
         }
 
