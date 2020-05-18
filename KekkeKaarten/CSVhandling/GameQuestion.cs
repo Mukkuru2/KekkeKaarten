cb0ca7a361e3b39efac9218f4caf8ac1fb853960
Mukkuru2 <dorusvanweelden@gmail.com>
2020-05-18 15:28:39 +0200

    Updates csvimporter and added question class to easily group questions.

---
+++ KekkeKaarten/CSVhandling/GameQuestion.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KekkeKaarten.CSVhandling
+{
+    public class GameQuestion
+    {
+        string missionID;
+        string id;
+        string question;
+        string didYouKnow;
+        string difficulty;
+        string comments;
+        string correctanswer;
+
+        public GameQuestion(string missionID, string id, string question, string didYouKnow, string difficulty, string comments, string correctanswer)
+        {
+            this.missionID = missionID;
+            this.id = id;
+            this.question = question;
+            this.didYouKnow = didYouKnow;
+            this.difficulty = difficulty;
+            this.comments = comments;
+            this.correctanswer = correctanswer;
+        }
+    }
+}
