467d095ca91d66066f7170d0494f403353032a2f
Stien_the_Troller <stijnkaay@gmail.com>
2020-05-28 10:12:20 +0200

    slime

---
+++ KekkeKaarten/GameObjects/Slime.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KekkeKaarten.GameObjects
+{
+    class Slime : Enemy
+    {
+        public Slime() : base()
+        {
+            //HP = 75;
+            //attack = 5;
+            //second for attack = 6
+        }
+        public void Direction(GameObject player)
+        {
+            if (player.Position.X >= position.X)
+            {
+                velocity.X = 64;
+            }
+            if (player.Position.X < position.X)
+            {
+                velocity.X = -64;
+            }
+            if (player.Position.Y >= position.Y)
+            {
+                velocity.Y = 64;
+            }
+            if (player.Position.Y < position.Y)
+            {
+                velocity.Y = -64;
+            }
+        }
+    }
+}
